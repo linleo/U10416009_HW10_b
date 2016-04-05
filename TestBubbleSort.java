3a402ed9c59902f94a9e70e10e7d4c1babb435ee
linleo <u10416009@go.utaipei.edu.tw>
2016-04-05 11:05:55 +0800

    Create TestBubbleSort.java
    User input the sequence of numbers, and do bubble sort.

---
+++ TestBubbleSort.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class TestBubbleSort
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		ArrayList<Integer> number = new ArrayList<>();
+		System.out.print("How many items you want to input ? ");
+		int item = input.nextInt();
+		System.out.print("Enter the sequence of numbers : ");
+		for (int i = 0; i < item; i++)
+			number.add(input.nextInt());
+		
+		BubbleSort bubbleSort = new BubbleSort();
+		bubbleSort.bubble(number);
+	}
+}
