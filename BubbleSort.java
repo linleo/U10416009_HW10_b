ecff2116176581e35d1198b30bd01ac55684f7f6
linleo <u10416009@go.utaipei.edu.tw>
2016-04-10 12:41:56 +0800

    Update BubbleSort.java
    Add comments and print descending order.

--- BubbleSort.java
+++ BubbleSort.java
@@ -8,12 +8,14 @@ public class BubbleSort
 	
 	void bubble(ArrayList<Integer> number)
 	{
+		//print original order
 		System.out.print("Data items in original order :");
 		for (i = 0; i < number.size(); i++) 
 		{
 			System.out.print(" " + number.get(i));
 		} 
-					  
+		
+		//do bubble sort			  
 		for (pass = 1; pass < number.size(); pass++) 
 		{                   														  
 			for (i = 0; i < number.size() - pass; i++) 
@@ -25,11 +27,21 @@ public class BubbleSort
 					number.set((i+1), buf);                               
 				}                                     
 			}                                 
-		}                                    
+		}
+		
+		//print ascending order
 		System.out.print("\nData items in ascending order :");
 		for (i = 0; i < number.size(); i++) 
 		{
 			System.out.print(" " + number.get(i));
 		}
+		
+		////print descending order
+		System.out.print("\nData items in descending order :");
+		for (i = number.size() - 1; i > -1; i--) 
+		{
+			System.out.print(" " + number.get(i));
+		}
+
 	}
 }
