3bf5eb0d5d92900fc193bfa08d900682605b2fc1
linleo <u10416009@go.utaipei.edu.tw>
2016-04-10 16:27:11 +0800

    Update TestBubbleSort.java
    Add comments.

--- TestBubbleSort.java
+++ TestBubbleSort.java
@@ -5,11 +5,14 @@ public class TestBubbleSort
 {
 	public static void main(String[] args)
 	{
+		
 		Scanner input = new Scanner(System.in);
 		ArrayList<Integer> number = new ArrayList<>();
+		//user input the total of items
 		System.out.print("How many items you want to input ? ");
 		int item = input.nextInt();
 		System.out.print("Enter the sequence of numbers : ");
+		//user input the sequence of numbers
 		for (int i = 0; i < item; i++)
 			number.add(input.nextInt());
 		
